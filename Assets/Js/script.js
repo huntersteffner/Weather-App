18d4e2bd084497211aad65eb5fbf57a8121a02a0
huntersteffner <huntersteffner@gmail.com>
2022-06-29 21:17:37 -0400

    It adds list of cities to history. Working on pulling data from API

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,1 +1,31 @@
 'use strict'
+
+const apiKey = '4e98480a953b623b3e964585552f2e66'
+
+console.log('test')
+
+fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.74&lon=84.38&appid=4e98480a953b623b3e964585552f2e66').then(res => res.json()).then(data => console.log(data))
+fetch('https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=4e98480a953b623b3e964585552f2e66').then(res => res.json()).then(data => console.log(data))
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=4e98480a953b623b3e964585552f2e66').then(res => res.json()).then(data => console.log(data))
+
+// let city = <div id="Atlanta" class="city">Atlanta</div>
+
+$('#submit-btn').on('click', function(e) {
+    let searchedCity = $('#search-bar').val()
+    const city = `<div id="${searchedCity}" class="city">${searchedCity}</div>`
+    $('#search-results').append(city)
+    $('#title').text(searchedCity)
+
+    const cityApi = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&appid=4e98480a953b623b3e964585552f2e66`
+    console.log(cityApi)
+
+    const fetchedObject = fetch(cityApi).then(res => res.json()).then(data => console.log(data))
+
+    e.preventDefault()
+})
+
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=miami&appid=4e98480a953b623b3e964585552f2e66').then(res => res.json()).then(data => console.log(data))
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Miami&appid=4e98480a953b623b3e964585552f2e66').then(res => res.json()).then(data => console.log(data))
+fetch('https://api.openweathermap.org/data/2.5/weather?q=MIAmi&appid=4e98480a953b623b3e964585552f2e66').then(res => res.json()).then(data => console.log(data))
+fetch('https://api.openweathermap.org/data/2.5/weather?q=mIAMI&appid=4e98480a953b623b3e964585552f2e66').then(res => res.json()).then(data => console.log(data))
