92d469d0450f9a540053021ce3acde7c9405eda2
huntersteffner <huntersteffner@gmail.com>
2022-06-30 21:04:37 -0400

    UV Index updates colors based on severity

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -2,15 +2,12 @@
 
 const apiKey = '4e98480a953b623b3e964585552f2e66'
 
-console.log('test')
-
 fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.74&lon=84.38&appid=4e98480a953b623b3e964585552f2e66').then(res => res.json()).then(data => console.log(data))
 fetch('https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=4e98480a953b623b3e964585552f2e66').then(res => res.json()).then(data => console.log(data))
 fetch('https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=4e98480a953b623b3e964585552f2e66').then(res => res.json()).then(data => console.log(data))
 
-// let city = <div id="Atlanta" class="city">Atlanta</div>
 
-const dateToday = moment().format(': MMMM Do, YYYY')
+const dateToday = moment().format('MMMM Do, YYYY')
 const datePlus1 = moment().add(1, 'days').format('MMMM Do YYYY')
 const datePlus2 = moment().add(2, 'days').format('MMMM Do YYYY')
 const datePlus3 = moment().add(3, 'days').format('MMMM Do YYYY')
@@ -23,6 +20,8 @@ $('#date3').text(datePlus3)
 $('#date4').text(datePlus4)
 $('#date5').text(datePlus5)
 
+$('#title').text(`Enter a City Name - ${dateToday}`)
+
 
 
 
@@ -30,7 +29,7 @@ $('#submit-btn').on('click', function(e) {
     let searchedCity = $('#search-bar').val()
     const city = `<div id="${searchedCity}" class="city">${searchedCity}</div>`
     $('#search-results').append(city)
-    $('#title').text(searchedCity + dateToday)
+    $('#title').text(`${searchedCity} - ${dateToday}`)
 
     const cityApi = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&units=imperial&appid=4e98480a953b623b3e964585552f2e66`
     console.log(cityApi)
@@ -43,9 +42,6 @@ $('#submit-btn').on('click', function(e) {
     }).then(function(data) {
         fetchedObject = data
         console.log(fetchedObject)
-        // $('#temp').text(fetchedObject.main.temp + '℉')
-        // $('#wind').text(fetchedObject.wind.speed + 'MPH')
-        // $('#humidity').text(fetchedObject.main.humidity)
         const cityApi2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${fetchedObject.coord.lat}&lon=${fetchedObject.coord.lon}&units=imperial&appid=4e98480a953b623b3e964585552f2e66`
         fetch(cityApi2).then(function(res) {
             return res.json().then(function(data) {
@@ -54,6 +50,17 @@ $('#submit-btn').on('click', function(e) {
                 $('#wind').text(data.current.wind_speed + 'MPH')
                 $('#humidity').text(data.current.humidity)
                 $('#uv').text(data.current.uvi)
+                console.log(data.current.uvi)
+                if(data.current.uvi < 4) {
+                    console.log('Safe')
+                    $('#uv').attr('class', 'uvSafe')
+                } else if (data.current.uvi > 6) {
+                    console.log('Dangerous')
+                    $('#uv').attr('class', 'uvDangerous')
+                } else {
+                    console.log('Medium')
+                    $('#uv').attr('class', 'uvMedium')
+                }
 
                 for(let i = 1; i < 6; i++) {
                     const curId = `#day${i}`
