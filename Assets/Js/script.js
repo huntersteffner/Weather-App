48a97c5a3ddb798923d616735a790115061e1c21
huntersteffner <huntersteffner@gmail.com>
2022-07-01 21:19:15 -0400

    Refactored. Created bug. Fixed bug

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,57 +1,48 @@
 'use strict'
 
-const apiKey = '4e98480a953b623b3e964585552f2e66'
-
-fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.74&lon=84.38&appid=4e98480a953b623b3e964585552f2e66').then(res => res.json()).then(data => console.log(data))
-fetch('https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=4e98480a953b623b3e964585552f2e66').then(res => res.json()).then(data => console.log(data))
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=4e98480a953b623b3e964585552f2e66').then(res => res.json()).then(data => console.log(data))
-
+// Mutable Variables
 let searchedCity
 let cityApi
 let fetchedObject
 let localStorageArray = []
 
-console.log(localStorage.recentSearches) 
-
-const storedSearches = JSON.parse(localStorage.getItem('recentSearches'))
+// Initial Local Storage pulls
+let storedSearches = JSON.parse(localStorage.getItem('recentSearches'))
 console.log(storedSearches)
-localStorageArray = storedSearches
+if(storedSearches === null) {
+    storedSearches = ['Atlanta']
+}
+localStorageArray = storedSearches 
 console.log(localStorageArray)
+// For Loop to have items in local storage populate
+    for(let i = 0;i < localStorageArray.length; i ++) {
+        if(localStorageArray[i] !== null) {
 
-for(let i = 0;i < localStorageArray.length; i ++) {
-    const city =`<div id="${localStorageArray[i]}" class="city">${localStorageArray[i]}</div>`
-    $('#search-results').append(city)
+            const city =`<div id="${localStorageArray[i]}" class="city">${localStorageArray[i]}</div>`
+            $('#search-results').append(city)
+        }
+
     
 }
-
+// Using Moment.js this provides the date variables for today's date and the five day forcast
 const dateToday = moment().format('MMMM Do, YYYY')
 const datePlus1 = moment().add(1, 'days').format('MMMM Do YYYY')
 const datePlus2 = moment().add(2, 'days').format('MMMM Do YYYY')
 const datePlus3 = moment().add(3, 'days').format('MMMM Do YYYY')
 const datePlus4 = moment().add(4, 'days').format('MMMM Do YYYY')
 const datePlus5 = moment().add(5, 'days').format('MMMM Do YYYY')
-
+// It puts the dates above in their corresponding fields
 $('#date1').text(datePlus1)
 $('#date2').text(datePlus2)
 $('#date3').text(datePlus3)
 $('#date4').text(datePlus4)
 $('#date5').text(datePlus5)
-
-const testArray = []
-
-testArray.push('Dog')
-testArray.push('Cat')
-testArray.push('Kow')
-
-console.log(testArray)
-
-
+// Sets the initial text in the title prior to searching any cities
 $('#title').text(`Enter a City Name - ${dateToday}`)
-
+// Variable for clicking on items in recent search history
 const recentSearchClick = function() {
     $('.city').on('click', function() {
         const cityCard = $(this).text()
-        console.log(cityCard)
         searchedCity = cityCard
         cityApi = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&units=imperial&appid=4e98480a953b623b3e964585552f2e66`
         
@@ -60,38 +51,34 @@ const recentSearchClick = function() {
     })
 }
 recentSearchClick()
-
+// Function used to pull data from Weather Api. It's saved as a variable here as it's called more than once
 const getWeather = function (cityApi, fetchedObject) {
-
     fetch(cityApi).then(function(res) {
         return res.json()
     }).then(function(data) {
         fetchedObject = data
-        console.log(fetchedObject)
+        // Due to the limitations in the api url that searches by cities, I pull the longitude and latitude coordinates from the api for the city and then make a second API call, because the call by latitude and longitude has more information than searching by the city
         const cityApi2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${fetchedObject.coord.lat}&lon=${fetchedObject.coord.lon}&units=imperial&appid=4e98480a953b623b3e964585552f2e66`
         fetch(cityApi2).then(function(res) {
             return res.json().then(function(data) {
-                console.log(data)
+                // Pull data for today's forecast
                 $('#temp').text(`Temp: ${data.current.temp} ℉`)
                 $('#wind').text(`Wind Speed: ${data.current.wind_speed} MPH`)
                 $('#humidity').text(`Humidity: ${data.current.humidity}%`)
                 $('#uv').text(`UV: ${data.current.uvi}`)
-                console.log(data.current.uvi)
+                // Determine the severity of UV index today and assign a corresponding class based on its severity
                 if(data.current.uvi < 4) {
-                    console.log('Safe')
                     $('#uv').attr('class', 'uvSafe')
                 } else if (data.current.uvi > 6) {
-                    console.log('Dangerous')
                     $('#uv').attr('class', 'uvDangerous')
                 } else {
-                    console.log('Medium')
                     $('#uv').attr('class', 'uvMedium')
                 }
+                // Add icon based on the weather conditions pulled directly from API
                 const iconToday = data.current.weather[0].icon 
                 const iconTodayUrl = `http://openweathermap.org/img/wn/${iconToday}@2x.png`
-                console.log(iconTodayUrl)
                 $('#icon').html(`<img src="${iconTodayUrl}" />`)
-    
+                // Pull information and icons for the five day forecast
                 for(let i = 1; i < 6; i++) {
                     const curId = `#day${i}`
                     const curTemp = `#temp${i}`
@@ -100,8 +87,6 @@ const getWeather = function (cityApi, fetchedObject) {
                     const curIcon = `#icon${i}`
                     const curIconObj = data.daily[i].weather[0].icon
                     const curIconUrl = `http://openweathermap.org/img/wn/${curIconObj}@2x.png`
-                    console.log(data.daily[i].temp.day)
-                    console.log(curId, curTemp)
                     $(curId).children(curIcon).html(`<img src="${curIconUrl}" />`)
                     $(curId).children(curTemp).text(`Temp: ${data.daily[i].temp.day} ℉`)
                     $(curId).children(curWind).text(`Wind: ${data.daily[i].wind_speed} MPH`)
@@ -109,30 +94,23 @@ const getWeather = function (cityApi, fetchedObject) {
                 }
             })
         })
-        console.log(cityApi2)
     })
 } 
-
+// Function when clicking the submit button
 $('#submit-btn').on('click', function(e) {
     searchedCity = $('#search-bar').val()
+    // Add to list of recent searches
     const city = `<div id="${searchedCity}" class="city">${searchedCity}</div>`
     $('#search-results').append(city)
     $('#title').text(`${searchedCity} - ${dateToday}`)
-
+    // Push new recent search to local storage
     localStorageArray.push(searchedCity)
-    console.log(localStorageArray)
-
     localStorage.setItem('recentSearches', JSON.stringify(localStorageArray))
-
-    let fetchedObject
-    
-    
+    // Set which city the API will search for
     let cityApi = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&units=imperial&appid=4e98480a953b623b3e964585552f2e66`
-
-
+    // What happens when you click on a recent searched item
     $('.city').on('click', function() {
         const cityCard = $(this).text()
-        console.log(cityCard)
         searchedCity = cityCard
         cityApi = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&units=imperial&appid=4e98480a953b623b3e964585552f2e66`
         
@@ -142,4 +120,3 @@ $('#submit-btn').on('click', function(e) {
     getWeather(cityApi, fetchedObject)
     e.preventDefault()
 })
-
